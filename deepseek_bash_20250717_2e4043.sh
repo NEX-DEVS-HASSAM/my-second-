b96ef056a9e0775b070f5b0db887c7ea64f492ef
phuddishuddi <bullilulli427@gmail.com>
2025-07-18 03:12:26 +0500

    Add files via upload
    this is my fiirst github

---
+++ deepseek_bash_20250717_2e4043.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/your-username/your-repo.git  # Downloads repo
+cd your-repo                                            # Moves into folder
+echo "Hello World!" > hello.txt                         # Creates a file
+git add .                                               # Stages changes
+git commit -m "My first change"                         # Saves changes
+git push                                                # Uploads to GitHub
